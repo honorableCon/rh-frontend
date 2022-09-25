0a300d0c9f216a266eb2ef79324de6859bbb0a13
Honorable con <honorablecon@gmail.com>
2022-09-25 12:07:35 +0000

    fix middleware

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -1,29 +1,34 @@
 import { NextResponse } from 'next/server';
 import { verify } from 'jsonwebtoken';
 
-const middleware = (req) => {
+const config = {
+  matcher: '/',
+};
+const middleware = async (req) => {
   const loginPath = '/auth/login';
   const { cookies } = req;
   const token = cookies.JWTtoken;
+  const publicKey = process.env.NEXT_PUBLIC_JWT_PUBLIC_KEY;
 
   if (token) {
     try {
-      const decoded = verify(token, process.env.NEXT_PUBLIC_JWT_PUBLIC_KEY);
+      const decoded = verify(token, publicKey);
       if (decoded) {
         return req.url == loginPath
           ? NextResponse.redirect('/')
-          : NextResponse.next(req);
+          : NextResponse.next();
       }
     } catch (err) {
       return NextResponse.redirect(loginPath);
     }
   }
 
-  if (req.url == loginPath) {
+  if (req.url.includes(loginPath)) {
+    console.log('*******' + req.url + '*******');
     return NextResponse.next();
   } else {
     return NextResponse.redirect(loginPath);
   }
 };
 
-export default middleware;
+export { middleware, config };
