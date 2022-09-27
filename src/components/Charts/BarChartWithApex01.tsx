bb1f924f8d78aa584b8a8169938ac735a8d8d99c
Honorable con <honorablecon@gmail.com>
2022-09-27 13:41:01 +0000

    fix charts

--- src/components/Charts/BarChartWithApex01.tsx
+++ src/components/Charts/BarChartWithApex01.tsx
@@ -1,68 +1,80 @@
-import ApexChart from "./ApexChart.tsx";
-import {useState} from 'react';
+import ApexChart from './ApexChart.tsx';
+import { useState } from 'react';
 
 const Bar01 = () => {
-
-    const [state, setState] = useState({
-    
-      series: [{
-        name: 'Net Profit',
-        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-      }, {
-        name: 'Free Cash Flow',
-        data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
-      }],
-      options: {
-        chart: {
-          type: 'bar',
-          height: 350
+  const [state, setState] = useState({
+    series: [
+      {
+        name: 'Permanent',
+        data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+      },
+      {
+        name: 'Interimaire',
+        data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+      },
+    ],
+    options: {
+      chart: {
+        type: 'bar',
+        height: 350,
+      },
+      colors: ['#7c4b08', '#ee820f'],
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '35%',
+          endingShape: 'rounded',
         },
-        colors:['#7c4b08', '#ee820f'],
-        plotOptions: {
-          bar: {
-            horizontal: false,
-            columnWidth: '35%',
-            endingShape: 'rounded'
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent'],
+      },
+      xaxis: {
+        categories: [
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+        ],
+      },
+      yaxis: {
+        title: {
+          text: 'employés',
+        },
+      },
+      fill: {
+        opacity: 1,
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return val + ' employés';
           },
         },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          show: true,
-          width: 2,
-          colors: ['transparent']
-        },
-        xaxis: {
-          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-        },
-        yaxis: {
-          title: {
-            text: '$ (thousands)'
-          }
-        },
-        fill: {
-          opacity: 1
-        },
-        tooltip: {
-          y: {
-            formatter: function (val) {
-              return "$ " + val + " thousands"
-            }
-          }
-        }
       },
-    });
+    },
+  });
 
-
-
-
-    return (
+  return (
     <div id="chart">
-      <ApexChart options={state.options} series={state.series} type="bar" height={350} />
+      <ApexChart
+        options={state.options}
+        series={state.series}
+        type="bar"
+        height={350}
+      />
     </div>
- );
-
-}
+  );
+};
 
 export default Bar01;
--- src/pages/statistiques/repartition-effectifs.js
+++ src/pages/statistiques/repartition-effectifs.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Bar from '../../components/Charts/BarChartWithApex.tsx';
 import Bar01 from '../../components/Charts/BarChartWithApex01.tsx';
 
 const repartitionEffectifs = () => {
   return (
     <div>
-      <Bar />
       <Bar01 />
     </div>
   );
